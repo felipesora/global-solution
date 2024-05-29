19b15a951276194a2f608a01115f43d8f1c5454e
felipesora <felipeusora@gmail.com>
2024-05-29 19:00:14 -0300

    página de riscos de não monitoramento, com html, css e js

---
+++ js/informações.js
@@ -0,0 +1,70 @@
+// botao 1  openModalAlert1 ModalAlert1 closeModalAlert1
+
+const openModalAlert1 = document.querySelector("#openModalAlert1");
+const ModalAlert1 = document.querySelector("#ModalAlert1");
+
+openModalAlert1.addEventListener("click", ()=>{
+    ModalAlert1.showModal();
+});
+
+const closeModalAlert1 = document.querySelector("#closeModalAlert1");
+closeModalAlert1.addEventListener("click", ()=>{
+    ModalAlert1.close();
+});
+
+
+// botao 2
+
+const openModalAlert2 = document.querySelector("#openModalAlert2");
+const ModalAlert2 = document.querySelector("#ModalAlert2");
+
+openModalAlert2.addEventListener("click", ()=>{
+    ModalAlert2.showModal();
+});
+
+const closeModalAlert2 = document.querySelector("#closeModalAlert2");
+closeModalAlert2.addEventListener("click", ()=>{
+    ModalAlert2.close();
+});
+
+// botao 3
+
+const openModalAlert3 = document.querySelector("#openModalAlert3");
+const ModalAlert3 = document.querySelector("#ModalAlert3");
+
+openModalAlert3.addEventListener("click", ()=>{
+    ModalAlert3.showModal();
+});
+
+const closeModalAlert3 = document.querySelector("#closeModalAlert3");
+closeModalAlert3.addEventListener("click", ()=>{
+    ModalAlert3.close();
+});
+
+// botao 4
+
+const openModalAlert4 = document.querySelector("#openModalAlert4");
+const ModalAlert4 = document.querySelector("#ModalAlert4");
+
+openModalAlert4.addEventListener("click", ()=>{
+    ModalAlert4.showModal();
+});
+
+const closeModalAlert4 = document.querySelector("#closeModalAlert4");
+closeModalAlert4.addEventListener("click", ()=>{
+    ModalAlert4.close();
+});
+
+// botao 5
+
+const openModalAlert5 = document.querySelector("#openModalAlert5");
+const ModalAlert5 = document.querySelector("#ModalAlert5");
+
+openModalAlert5.addEventListener("click", ()=>{
+    ModalAlert5.showModal();
+});
+
+const closeModalAlert5 = document.querySelector("#closeModalAlert5");
+closeModalAlert5.addEventListener("click", ()=>{
+    ModalAlert5.close();
+});
